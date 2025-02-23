53853ece8f51ec00fb829a8630da243a5ed7ae40
mo7ammedd <medo.mostafa22255@gmail.com>
2025-02-23 20:52:25 +0200

    add palastine line

---
+++ app/blog/difference-between-cluster-and-non-cluster-index/page.tsx
@@ -0,0 +1,50 @@
+import { Metadata } from 'next'
+import { BLOG_POSTS } from '../../data'
+
+type BlogProps = {
+  params: {
+    slug: string
+  }
+}
+
+export async function generateMetadata({ params }: BlogProps): Promise<Metadata> {
+  // Find the matching blog post from the data
+  const post = BLOG_POSTS.find(
+    (post) => post.link === `/blog/${params.slug}`
+  )
+
+  if (!post) {
+    return {
+      title: 'Blog Post Not Found',
+      description: 'The requested blog post could not be found.'
+    }
+  }
+
+  return {
+    title: `${post.title} | Mohammed's Blog`,
+    description: post.description,
+    openGraph: {
+      type: 'article',
+      title: post.title,
+      description: post.description,
+      url: `https://www.mohammedd.tech/blog/${params.slug}`,
+      siteName: "Mohammed's Blog",
+      images: [
+        {
+          url: '/og-image.png', // You can make this dynamic per post if needed
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      site: '@mohamedtv',
+      creator: '@mohamedtv',
+      title: post.title,
+      description: post.description,
+      images: ['/og-image.png'], // You can make this dynamic per post if needed
+    },
+  }
+}
